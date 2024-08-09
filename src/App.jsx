03d8b5cf245faf414f0d045393c443ed6325128c
Dylan Petzer <54238036+CodingDyl@users.noreply.github.com>
2024-08-09 17:00:22 +0200

    updated hero sectiohn

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,20 @@
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import MainSection from './pages/MainSection'
 import JobsSection from './pages/JobsSection'
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
 
 function App() {
 
   return (
+    <MantineProvider>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainSection />} />
         <Route path="/projects" element={<JobsSection />} />
       </Routes>
     </BrowserRouter>
+    </MantineProvider>
   )
 }
 
--- src/components/MainHero.tsx
+++ src/components/MainHero.tsx
@@ -2,23 +2,23 @@
 import React from "react";
 import { ShootingStars } from "./ui/shooting-stars";
 import { StarsBackground } from "./ui/stars-background";
-import { FaArrowDown } from "react-icons/fa";
+import { FaArrowRight } from "react-icons/fa";
+import { Review } from "./Review";
+import { Button } from "@mantine/core";
 
 export function MainHero() {
   return (
-    <div className="min-h-[100vh] rounded-md bg-neutral-900 flex flex-col gap-10 items-center justify-center relative w-full px-[20%]">
-        <p className="hidden md:block text-slate-400 text-center leading-6 text-3xl max-w-3xl">
-        Do what you do best, we'll handle the marketing.
-        </p>
-      <h2 className="relative flex-col md:flex-row z-10 text-3xl md:text-8xl md:leading-tight max-w-8xl mx-auto text-center tracking-tight font-medium bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-white to-white flex items-center gap-2 md:gap-8">
-        Transform Your Marketing
+    <div className="min-h-[110vh] rounded-md bg-neutral-900 flex flex-col gap-10 items-center justify-center relative w-full px-[10%]">
+      <h2 className="relative flex-col md:flex-row z-10 text-3xl md:text-7xl lg:text-8xl md:leading-tight max-w-8xl mx-auto text-center tracking-tight font-medium bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-white to-white flex items-center gap-2 md:gap-8">
+        Transform Your Marketing with ThreeSixty
       </h2>
-      <button className="hidden md:block rounded-md text-3xl bg-sky-500 px-6 py-3 hover:-translate-y-2 text-black z-2">Book a Consultation</button>
+      <p className="hidden md:block text-white text-center leading-6 text-xl max-w-2xl">
+      Automate Campaigns, Engage Audiences, and Boost Lead Generation with Our All-in-One Marketing Solution
+        </p>
+      <Review />
+      <Button variant="filled" size="lg" radius="md" rightSection={<FaArrowRight size={14} />}>Book a Consultation</Button>
       <ShootingStars />
       <StarsBackground />
-      <div className="absolute bottom-8 left-1/2 transform -translate-x-1/2">
-        <FaArrowDown className="text-white text-3xl md:text-4xl lg:text-5xl animate-bounce" />
-      </div>
     </div>
   );
 }
